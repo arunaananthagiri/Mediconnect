511ba09097cb880bcdcc7077cc5e06ece4e1cd3b
arunaananthagiri <arunaananthagiri04@gmail.com>
2025-02-16 22:46:08 +0530

    first

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+
+const port = process.env.PORT || 5001;
+
+console.log(process.env.MONGO_URL);
+
+app.listen(port, () => console.log(`Node server started at port : ${port}`));
